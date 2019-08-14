7121e5d49a64dd5d824b2cca17ceccf120b9e094
digaus <di.gaus@gmx.de>
2019-08-14 20:46:20 +0200

    Change definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,11 @@
-declare global {
+import { PushNotificationActionPerformed, PluginListenerHandle } from '@capacitor/core'
+declare module '@capacitor/core/dist/esm/core-plugin-definitions' {
   interface PluginRegistry {
     BackgroundFCM: BackgroundFCMPlugin;
   }
 }
 
-export interface BackgroundFCMPlugin {
+export interface BackgroundFCMPlugin  {
   setAdditionalData(options: { value: string }): Promise<{value: string}>;
+  addListener(eventName: 'pushNotificationActionPerformed', listenerFunc: (notification: PushNotificationActionPerformed) => void): PluginListenerHandle;
 }
